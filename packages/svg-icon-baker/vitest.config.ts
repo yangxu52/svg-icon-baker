30ad7202ec6bb6b19b87d1f6be156cc4e05e08fb
yangxu52 <yyswj10@163.com>
2025-11-01 12:15:44 +0800

    chore: standardize unit test

--- packages/svg-icon-baker/vitest.config.ts
+++ packages/svg-icon-baker/vitest.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
     environment: 'node',
-    include: ['src/__tests__/**/*.spec.ts'],
+    include: ['src/**/*.test.ts'],
   },
 })
