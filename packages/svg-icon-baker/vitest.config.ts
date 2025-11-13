45bc15ddc90d606f9279f3a196eb8a6f127d0329
yangxu52 <yyswj10@163.com>
2025-11-13 17:06:59 +0800

    chore: test add coverage

--- packages/svg-icon-baker/vitest.config.ts
+++ packages/svg-icon-baker/vitest.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
   },
 })
